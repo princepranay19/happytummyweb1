6592eb38110bf9b5f425c7ae12fd0ae86006cc10
akashardalkar <akashardalkar1@gmail.com>
2024-08-15 13:41:23 +0530

    happy tummy

---
+++ js/form1.js
@@ -0,0 +1,13 @@
+
+document.getElementById("request").addEventListener("submit", function (e) {
+	e.preventDefault();
+  
+	var formData = new FormData(form);
+	// output as an object
+	console.log(Object.fromEntries(formData));
+  
+	// ...or iterate through the name-value pairs
+	for (var pair of formData.entries()) {
+	  console.log(pair[0] + ": " + pair[1]);
+	}
+  });
